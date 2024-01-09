0c8af34303f03d4c7316e9c5e803173ba87c5906
JuanZam <you@example.com>
2024-01-09 14:08:28 -0500

    Add coment to my code to make it readable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Import library to read CSV files
 import csv
 
+# Open it and logic to read only temperature data
 with open("weather_data.csv", "r") as data_file:
     data = csv.reader(data_file)
     temperatures = []
