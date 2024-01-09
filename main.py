988cec2d8f6f1cc2f27fb7ced8cae3289dec6575
JuanZam <you@example.com>
2024-01-09 14:06:39 -0500

    New code

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ with open("weather_data.csv", "r") as data_file:
     data = csv.reader(data_file)
     temperatures = []
     for row in data:
-        if row[1] != "temp"
+        if row[1] != "temp":
             temperatures.append(int(row[1]))
     print(temperatures)
