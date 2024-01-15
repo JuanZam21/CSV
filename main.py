28ba7d016f8be3e6a4fdf2560e2e61afb32ab1e3
JuanZam <you@example.com>
2024-01-14 20:04:34 -0500

    pandas

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # Import library to read CSV files
-import csv
+import pandas as pd
 
 # Open it and logic to read only temperature data
-with open("weather_data.csv", "r") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+data = pd.read_csv("weather_data.csv")
+
+print(data['temp'])
